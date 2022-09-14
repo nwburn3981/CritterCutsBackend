9de4c5e2fca9d0f39d81045a7513e640ea9f39d0
gortiz2021 <iGabriel661@hotmal.com>
2022-09-14 09:45:56 -0700

    Update

--- src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
+++ src/main/java/com/cognixia/jump/config/SecurityConfiguration.java
@@ -1,5 +1,74 @@
 package com.cognixia.jump.config;
 
 public class SecurityConfiguration {
+<<<<<<< Updated upstream
     
+=======
+	
+	@Autowired
+	UserDetailsService userDetailsService;
+	
+	@Autowired
+	JwtRequestFilter jwtRequestFilter;
+	
+	
+	// Authentication - who are you?
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		
+		return userDetailsService;
+	}
+	
+	// Authorization - what do you want?
+	@Bean
+	protected SecurityFilterChain filterChain( HttpSecurity http ) throws Exception {
+		
+		http.csrf().disable()
+			.authorizeRequests()
+		//	.antMatchers("/api/**").hasRole("USER") // have to be user role to access /hello
+			
+			.antMatchers("/api/appointments").permitAll()
+			
+			.antMatchers("/create/customer").permitAll()
+			.antMatchers("/authenticate").permitAll() // allow anyone to create token
+			//.anyRequest().authenticated()  //any other API in this project has to be authenticated (token or user info to access)
+			.and()
+				.sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ); // tell spring security to NOT CREATE sessions
+		
+		// this request will go through many filters, make sure that the FIRST filter that is checked is
+		// the filter for jwts, in order to make sure of that, the filter has to be checked before you check the 
+		// username & password (filter)
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+				 
+		return http.build();
+	}
+	
+	
+	// Encoder -> method that will encode/decode all the user passwords
+	@Bean
+	protected PasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+	// load the encoder & user details service that are needed for spring security to do authentication
+	@Bean
+	protected DaoAuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder( encoder() );
+		
+		return authProvider;
+	}
+	
+	// can autowire and access the authentication manager (manages authentication (login) of our project)
+	@Bean
+	protected AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+	
+>>>>>>> Stashed changes
 }
---
+++ src/main/java/com/cognixia/jump/model/User.java
@@ -0,0 +1,98 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class User implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long user_id;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private boolean enabled;
+	
+	@OneToOne(mappedBy = "user")
+	private Customer customer;
+	
+	//Once employee model is created this will be where it relates to User
+	@OneToOne(mappedBy = "user")
+	private Employee employee;
+	
+	public User () {
+		this(-1L, "", "", false);
+	}
+
+	public User(Long user_id, String username, String password, boolean enabled) {
+		super();
+		this.user_id = user_id;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public Long getUser_id() {
+		return user_id;
+	}
+	
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
+				+ ", customer=" + customer + "]";
+	}
+	
+
+
+}
