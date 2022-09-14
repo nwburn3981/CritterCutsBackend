7d99ba8f02266aba29021a9b0d829c678c4080d0
gortiz2021 <iGabriel661@hotmal.com>
2022-09-14 09:33:40 -0700

    New repo for employee and user

---
+++ src/main/java/com/cognixia/jump/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.cognixia.jump.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cognixia.jump.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
