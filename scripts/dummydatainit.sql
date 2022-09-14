e8ac792f6962391cc528bf0a4e0328da7a1fdc59
jlaw52 <jacob.a.laws@gmail.com>
2022-09-14 15:09:17 -0500

    updated sql script
    no longer needs extra useless values

--- scripts/dummydatainit.sql
+++ scripts/dummydatainit.sql
@@ -9,9 +9,14 @@
 use critter_cuts;
 insert into user values (1, true, "password", "username");
 insert into customer values (1, "customer@gmail.com", "John Doe", 1235556789, "1999-01-01", "password", "ROLE_USER", "username", 1);
-insert into pet_info values (1, 5, "Shiba", true, "Spot", "Dog", 1, 1);
+insert into pet_info values (1, 5, "Shiba", true, "Spot", "Dog", 1);
 insert into user values (2, true, "pass", "user");
 insert into employee values (1, "1999-01-01", "Jane Doe", "ROLE_ADMIN", 50000, "Dogs", 2);
-insert into appointment values (1, "Jane Doe", "2022-01-01", 100.99, "Routine Groom", "12:00:00", 1, 1, 1);
+insert into appointment values (1, "Jane Doe", "2022-01-01", 100.99, "Routine Groom", "12:00:00", 1, 1);
 
-select * from critter_cuts.appointment;
+-- you should now be able to add more than one pet per owner and more than one appointment per pet
+insert into pet_info values (2, 12, "American Shorthair", false, "Gooey Butter Cake", "Guinea Pig", 1);
+insert into appointment values (2, "Jane Doe", "2022-02-02", 199.99, "Premium Groom", "20:00:00", 1, 1);
+
+-- select * from appointment;
+-- select * from pet_info;
