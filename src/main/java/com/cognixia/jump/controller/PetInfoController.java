e401f3235464d4c467b792b19466c6a1dff927e2
jlaw52 <jacob.a.laws@gmail.com>
2022-09-15 15:17:49 -0500

    added openapi annotations to controllers
    Appointment, Customer, Employee, Pet, and User controllers
    
    Also, user controller has redundant methods, one should be deleted

--- src/main/java/com/cognixia/jump/controller/PetInfoController.java
+++ src/main/java/com/cognixia/jump/controller/PetInfoController.java
@@ -21,6 +21,9 @@ import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.PetInfo;
 import com.cognixia.jump.service.PetInfoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RequestMapping("/api")
 @RestController
 public class PetInfoController {
@@ -28,16 +31,34 @@ public class PetInfoController {
 	@Autowired
 	PetInfoService petService;
 	
+	@Operation(summary = "Find all pets", description = "Finds every pet in the database.")
+	@ApiResponse(responseCode = "200", description = "All pets have been found")
+	@ApiResponse(responseCode = "400", description = "Pets not found")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@GetMapping("/petInfo")
 	public List<PetInfo> getAllPets(){
 		return petService.getAllPetInfo();
 	}
 	
+	@Operation(summary = "Finds the pet by the id", description = "Searches the database for a pet which has the id matching the one given by the uri")
+	@ApiResponse(responseCode = "200", description = "Pet has been found")
+	@ApiResponse(responseCode = "400", description = "Pet not found")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Pet not found")
 	@GetMapping("/petInfo/id/{pet_id}")
 	public PetInfo getPet(@PathVariable long pet_id) throws ResourceNotFoundException {
 		return petService.getPetById(pet_id);
 	}
 	
+	@Operation(summary = "Creates a new pet", description = "Posts raw JSON data to be inserted into the database with all of the values needed to create a new pet")
+	@ApiResponse(responseCode = "201", description = "Pet has been created")
+	@ApiResponse(responseCode = "400", description = "Pet not created")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PostMapping("/petInfo/add/petInfo")
 	public ResponseEntity<PetInfo>createPetInfo(@Valid @RequestBody PetInfo petInfo){
 		
@@ -46,12 +67,25 @@ public class PetInfoController {
 		return new ResponseEntity<>(added, HttpStatus.CREATED);
 	}
 	
+	
+	@Operation(summary = "Updates an existing pet", description = "Updates all fields in a pet, puts JSON raw data that includes all pet information")
+	@ApiResponse(responseCode = "200", description = "Pet updated")
+	@ApiResponse(responseCode = "400", description = "Pet not updated")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PutMapping("/petInfo/update/petInfo")
 	public ResponseEntity<PetInfo> updatePetInfo(@Valid @RequestBody PetInfo petInfo) throws ResourceNotFoundException{
 		PetInfo updated = petService.updatePetInfo(petInfo);
 		return new ResponseEntity<>(updated, HttpStatus.OK);
 	}
 	
+	@Operation(summary = "Deletes an existing pet", description = "Removes an existing pet from the database, the database will be searched for a pet with a matching id and it will be removed from the database permanently")
+	@ApiResponse(responseCode = "200", description = "Pet deleted")
+	@ApiResponse(responseCode = "400", description = "Pet not deleted")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@DeleteMapping("/petInfo/delete/{pet_id}")
 	public ResponseEntity<PetInfo> removePet(@PathVariable long pet_id) throws ResourceNotFoundException{
 		PetInfo deleted = petService.deletePetInfo(pet_id);
@@ -59,6 +93,14 @@ public class PetInfoController {
 		return new ResponseEntity<>(deleted, HttpStatus.OK);
 	}
 	
+	@Operation(summary = "Finds a pet by the id of its owner", description = "The database will be searched for a pet that has a customer_id matching the id given and retrieves it")
+	@ApiResponse(responseCode = "200", description = "Pet fetched, bow wow")
+	@ApiResponse(responseCode = "400", description = "Pet not found")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
+	@ApiResponse(responseCode = "418", description = "fortnite")
+	@ApiResponse(responseCode = "429", description = "STOP THAT")
 	@GetMapping("/petInfo/pet_customerid/{id}")
 	public PetInfo findPetbyCustomerId(@PathVariable Long id) throws ResourceNotFoundException{
 		return petService.findPetbyCustomerId(id);
--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -22,6 +22,9 @@ import com.cognixia.jump.model.Customer;
 import com.cognixia.jump.model.User;
 import com.cognixia.jump.repository.UserRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 @RequestMapping("/api")
 @RestController
 public class UserController {
@@ -32,16 +35,34 @@ public class UserController {
 	@Autowired
 	PasswordEncoder encoder;
 
+	@Operation(summary = "Find all users", description = "Finds every user in the database.")
+	@ApiResponse(responseCode = "200", description = "All users have been found")
+	@ApiResponse(responseCode = "400", description = "Users not found")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		return repo.findAll();
 	}
 
+	@Operation(summary = "Finds the user by the id", description = "Searches the database for a user which has the id matching the one given by the uri")
+	@ApiResponse(responseCode = "200", description = "User has been found")
+	@ApiResponse(responseCode = "400", description = "User not found")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "User not found")
 	@GetMapping("/users/{id}")
 	public Optional<User> getUserById(@PathVariable long id) {
 		return repo.findById(id);
 	}
 
+	@Operation(summary = "Creates a new user", description = "Posts raw JSON data to be inserted into the database with all of the values needed to create a new user")
+	@ApiResponse(responseCode = "201", description = "User has been created")
+	@ApiResponse(responseCode = "400", description = "User not created")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PostMapping("/users/new")
 	public User addNewUser(User user) {
 		user.setUser_id(1L);
@@ -51,6 +72,12 @@ public class UserController {
 		return saved;
 	}
 
+	@Operation(summary = "Updates an existing user", description = "Updates all fields in a user, puts JSON raw data that includes all pet information")
+	@ApiResponse(responseCode = "200", description = "User updated")
+	@ApiResponse(responseCode = "400", description = "User not updated")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PutMapping("/users/update")
 	public User updateUser(User user) throws ResourceNotFoundException {
 
@@ -64,6 +91,12 @@ public class UserController {
 		return saved;
 	}
 
+	@Operation(summary = "Finds the user by username")
+	@ApiResponse(responseCode = "201", description = "success")
+	@ApiResponse(responseCode = "400", description = "failure")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PostMapping("/users/details")
 	public ResponseEntity<?> getUserDetails(@RequestBody AuthenticationRequest request) {
 
@@ -76,12 +109,25 @@ public class UserController {
 		}
 	}
 
+	@Operation(summary = "Deletes an existing user", description = "Removes an existing user from the database, the database will be searched for a user with a matching id and it will be removed from the database permanently")
+	@ApiResponse(responseCode = "200", description = "User deleted")
+	@ApiResponse(responseCode = "400", description = "User not deleted")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@DeleteMapping("/users/delete/{id}")
 	public Optional<User> deleteUser(@PathVariable Long id) {
 		repo.deleteById(id);
 		return repo.findById(id);
 	}
-	
+
+	//TODO: /create/user and /users/new enpoints serve the same purpose, remove one
+	@Operation(summary = "Creates a new user", description = "Posts raw JSON data to be inserted into the database with all of the values needed to create a new user")
+	@ApiResponse(responseCode = "201", description = "User has been created")
+	@ApiResponse(responseCode = "400", description = "User not created")
+	@ApiResponse(responseCode = "401", description = "Forbidden: unauthorized")
+	@ApiResponse(responseCode = "403", description = "Forbidden: unauthorized 2: electric boogaloo")
+	@ApiResponse(responseCode = "404", description = "Page not found")
 	@PostMapping("/create/user")
 	public ResponseEntity<?> createUser( @RequestBody User user ) {
 		
